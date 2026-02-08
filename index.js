be0dde0033d5777510a93282465d0d813e1eb51a
jeerarit-cell <jeerarit@gmail.com>
2026-02-08 16:19:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-test
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Game Server is running");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server started on port", PORT);
+});
