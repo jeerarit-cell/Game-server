6a053da7f5dd638d3533c77ec873a84e5ea40b63
jeerarit-cell <jeerarit@gmail.com>
2026-02-08 16:48:05 +0700

    connect firestore

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from "express";
+import admin from "firebase-admin";
 
 const app = express();
+app.use(express.json());
 
+// 🔑 โหลด key จาก ENV
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+// test route
 app.get("/", (req, res) => {
-  res.send("Game Server is running");
+  res.send("Game Server + Firestore connected");
 });
 
 const PORT = process.env.PORT || 3000;
